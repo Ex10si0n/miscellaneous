49129bd38f97b85bdaf87d0f429159c51a76e3b4
Ex10si0n <yzb.ex10si0n@icloud.com>
2020-05-22 19:53:16 +0800

    update

---
+++ WorkSpaces/java-learn/src/studio/aspire/javalearn/TestDate.java
@@ -0,0 +1,10 @@
+package studio.aspire.javalearn;
+
+import java.util.Date;
+
+public class TestDate {
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+    }
+}
