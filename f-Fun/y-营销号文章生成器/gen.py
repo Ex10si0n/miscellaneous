40fdf538fbaa0b86be412611b023d4dc7c6e8d6e
Ex10si0n <yzb.ex10si0n@icloud.com>
2020-06-17 14:18:59 +0800

    Update

--- f-Fun/y-营销号文章生成器/gen.py
+++ f-Fun/y-营销号文章生成器/gen.py
@@ -1,4 +1,6 @@
+import pygame
 import random
+import pyttsx3
 
 class WordDict:
     greeting = ['Hello', 'Hello小伙伴们', '小伙伴们']
@@ -14,10 +16,18 @@ class ParaStrut:
         a = random.randint(0, len(WordDict.greeting)-1)
         b = random.randint(0, len(WordDict.intro)-1)
         c = random.randint(0, len(WordDict.writer_name)-1)
-        print('%s大家好，%s%s。%s是怎么回事呢?%s相信大家都很熟悉，但是%s是怎么回事呢?下面就让小编带大家一起了解吧。%s，其实就是%s，大家可能会感到很惊讶，但事实就是这样，小编也感到非常惊讶。那么这就是关于%s的事情了，大家有什么想法呢？欢迎在评论区告诉小编一起讨论哦。视频资料来自互联网，这里是%s，我们下期再见，拜拜。'\
+        p = ('%s大家好，%s%s。%s是怎么回事呢?%s相信大家都很熟悉，但是%s是怎么回事呢?下面就让小编带大家一起了解吧。%s，其实就是%s，大家可能会感到很惊讶，但事实就是这样，小编也感到非常惊讶。那么这就是关于%s的事情了，大家有什么想法呢？欢迎在评论区告诉小编一起讨论哦。视频资料来自互联网，这里是%s，我们下期再见，拜拜。'\
             % (WordDict.greeting[a], WordDict.intro[b], WordDict.writer_name[c], WordDict.topic[0],\
             WordDict.topic[0], WordDict.topic[0], WordDict.topic[0], WordDict.topic[1],\
             WordDict.topic[0], WordDict.writer_name[c]))
+        engine = pyttsx3.init()
+        pygame.mixer.init()
+        track = pygame.mixer.music.load("trip.mp3")
+        pygame.mixer.music.play()
+        engine.say(p)
+        print(p)
+        engine.runAndWait()
+
 
 if __name__ == '__main__':
     ParaStrut.getInput()
---
+++ f-Fun/y-营销号文章生成器/speech.py
@@ -0,0 +1,6 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("你好世界")
+
+engine.runAndWait()
